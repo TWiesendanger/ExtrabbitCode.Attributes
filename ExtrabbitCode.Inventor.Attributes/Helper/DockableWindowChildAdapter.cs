d3cffed53a1d336135c515a946cc8899ad3ce7dd
Tobias Wiesendanger <tobias.wiesendanger@mum.ch>
2026-04-17 23:27:07 +0200

    fix: revert to old adapter behavior

--- ExtrabbitCode.Inventor.Attributes/Helper/DockableWindowChildAdapter.cs
+++ ExtrabbitCode.Inventor.Attributes/Helper/DockableWindowChildAdapter.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
 namespace ExtrabbitCode.Inventor.Attributes.Helper;
 
-public static partial class DockableWindowChildAdapter
+public static class DockableWindowChildAdapter
 {
     private const uint DlgcWantArrows = 0x0001;
     private const uint DlgcWantTab = 0x0002;
@@ -14,62 +13,27 @@ public static partial class DockableWindowChildAdapter
     private const uint DlgcWantChars = 0x0080;
     private const int WmGetDlgCode = 0x0087;
 
-    private const int GwlExStyle = -20;
-    private const int WsExToolWindow = 0x00000080;
-    private const int WsExAppWindow = 0x00040000;
-
-    [LibraryImport("user32.dll", SetLastError = true)]
-    [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-    private static partial int GetWindowLong(IntPtr hWnd, int nIndex);
-
-    [LibraryImport("user32.dll", SetLastError = true)]
-    [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-    private static partial int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-        
     public static void AddWpfWindow(DockableWindow? dockableWindow, Window? window)
     {
-        if (dockableWindow == null || window == null)
+        if (window == null)
         {
             return;
         }
 
-        PrepareWindow(window);
+        window.WindowStyle = WindowStyle.None;
+        window.WindowState = WindowState.Maximized;
+        window.ResizeMode = ResizeMode.NoResize;
+        window.ShowInTaskbar = false;
+        window.Show();
 
-        WindowInteropHelper interopHelper = new(window)
-        {
-            Owner = new IntPtr(Globals.InvApp.MainFrameHWND)
-        };
-
-        if (!window.IsVisible)
-        {
-            window.Show();
-        }
-
+        Window actualWindow = Window.GetWindow(window) ?? window;
+        WindowInteropHelper interopHelper = new(actualWindow);
         IntPtr hwnd = interopHelper.EnsureHandle();
 
-        RemoveAltTabPresence(hwnd);
-
-        dockableWindow.AddChild(hwnd.ToInt64());
+        dockableWindow?.AddChild(hwnd.ToInt64());
 
         HwndSource? hwndSource = HwndSource.FromHwnd(hwnd);
         hwndSource?.AddHook(WndProc);
-    }
-
-    private static void PrepareWindow(Window window)
-    {
-        window.WindowStyle = WindowStyle.None;
-        window.ResizeMode = ResizeMode.NoResize;
-        window.ShowInTaskbar = false;
-        window.WindowStartupLocation = WindowStartupLocation.Manual;
-        window.Topmost = false;
-    }
-
-    private static void RemoveAltTabPresence(IntPtr hwnd)
-    {
-        int exStyle = GetWindowLong(hwnd, GwlExStyle);
-        exStyle &= ~WsExAppWindow;
-        exStyle |= WsExToolWindow;
-        SetWindowLong(hwnd, GwlExStyle, exStyle);
     }
 
     private static IntPtr WndProc(
