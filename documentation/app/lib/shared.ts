f6b888352abf7e35814bce0f698afe77a57ddad8
Tobias Wiesendanger <tobias.wiesendanger@mum.ch>
2026-04-20 23:16:19 +0200

    docu: added initial documentation draft

---
+++ documentation/app/lib/shared.ts
@@ -0,0 +1,11 @@
+export const appName = 'Extrabbitcode Inventor Attributes';
+export const docsRoute = '/docs';
+export const docsImageRoute = '/og/docs';
+export const docsContentRoute = '/llms.mdx/docs';
+
+// fill this with your actual GitHub info, for example:
+export const gitConfig = {
+  user: 'TWiesendanger',
+  repo: 'Extrabbitcode.Inventor.Attributes',
+  branch: 'main',
+};
